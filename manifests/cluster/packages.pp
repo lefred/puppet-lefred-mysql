fa697d27d9fd7bd16f33301cd9fcbf18cbdf41a7
frederic.descamps <frederic.descamps@percona.com>
2014-06-23 23:24:29 +0200

    trying to fix the requirement differences between PXC and MariaDB Galera Cluster
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/cluster/packages.pp
+++ manifests/cluster/packages.pp
@@ -3,14 +3,12 @@ class mysql::cluster::packages {
   if  $mysql::mysql_distro == "mariadb" {
         $packs = [ "MariaDB-Galera-server", rsync ]
         $mysql_bin = "mysql"
-        $packs_galera = "" 
+        $require = Yumrepo['mysql-repo'] 
   } elsif  $mysql::mysql_distro == "percona" {
         $packs = [ "Percona-XtraDB-Cluster-server-${mysql::mysql_ver}", "Percona-XtraDB-Cluster-client-${mysql::mysql_ver}", "Percona-Server-shared-compat", "rsync" ]
         $packs_galera = [ "Percona-XtraDB-Cluster-galera-${mysql::galera_version}" ]
         $mysql_bin = "mysql"
-        info("packs_galera: $packs_galera")
-        info("packs: $packs")
-        info("require_pack: $require_pack")
+        $require = Package[$packs_galera]
         package {
             $packs_galera:
                     require => Yumrepo['mysql-repo'],
@@ -22,7 +20,7 @@ class mysql::cluster::packages {
 
   package {
         $packs:
-                    require => [ Yumrepo['mysql-repo'], Package[$packs_galera] ],
+                    require => $require;
                     ensure  => "installed";
   }
 
