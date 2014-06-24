b2ceb35c354b76373bae9b325caa9a3c733e51ff
frederic.descamps <frederic.descamps@percona.com>
2014-06-24 11:38:59 +0200

    add parameter to define the bind interface
    Signed-off-by: frederic.descamps <frederic.descamps@percona.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class mysql ($mysql_distro="community", $mysql_version="5.5", $mysql_serverid=undef, $ensure="running", $bootstrap=undef, $galera_version=2) {
+class mysql ($mysql_distro="community", $mysql_version="5.5", $mysql_serverid=undef, $ensure="running", $bootstrap=undef, $galera_version=2,  $mysql_bind_interface="eth0") {
         
         if $mysql::mysql_version == "5.7" {
             info("Your are brave ! Using 5.7 !!")
@@ -13,6 +13,10 @@ class mysql ($mysql_distro="community", $mysql_version="5.5", $mysql_serverid=un
             info("You go to something new...  ! Using 10 !!")
             $mysql_ver="10.0"
         }
+
+
+        $mysql_ip = inline_template("<% scope.lookupvar('::ipaddress_${mysql_bind_interface}') -%>")
+
 
         include mysql::repository
 
