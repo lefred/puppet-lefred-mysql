6e5ab0b9aedbe7a74392f3968c1bd95c8f4212a6
Frédéric -lefred- Descamps <lefred@percona.com>
2014-07-15 14:01:54 +0200

    no selinux for debian
    Signed-off-by: Frédéric -lefred- Descamps <lefred@percona.com>

--- manifests/cluster/config.pp
+++ manifests/cluster/config.pp
@@ -8,12 +8,16 @@ class mysql::cluster::config  {
                         ensure  => present,
                         content => template("mysql/cluster/my.cnf.erb"),
         }
-
-        exec {
-                "disable-selinux":
-                        path    => ["/usr/bin","/bin"],
-                        command => "echo 0 >/selinux/enforce",
-                        unless => "grep 0 /selinux/enforce",
+       
+        case $::osfamily {
+          'RedHat': {
+        	exec {
+                	"disable-selinux":
+                        	path    => ["/usr/bin","/bin"],
+                        	command => "echo 0 >/selinux/enforce",
+                        	unless => "grep 0 /selinux/enforce",
+        	}
+	   }
         }
 
 }
