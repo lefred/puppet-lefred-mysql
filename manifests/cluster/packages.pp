63a787e9000ac9cc30ee801ffb6757a3dd477109
Frédéric -lefred- Descamps <lefred@percona.com>
2014-07-15 14:40:05 +0200

    5.6 needs testing currently
    Signed-off-by: Frédéric -lefred- Descamps <lefred@percona.com>

--- manifests/cluster/packages.pp
+++ manifests/cluster/packages.pp
@@ -21,14 +21,7 @@ class mysql::cluster::packages {
                	$require_loc = Yumrepo['mysql-repo'] 
           }
           'Debian': {
-                if $mysql::mysql_ver == "56" {
-                  $pxc_server = "percona-xtradb-cluster-server"
-                  $pxc_client = "percona-xtradb-cluster-client"
-                } else {
-                  $pxc_server = "percona-xtradb-cluster-server-${mysql::mysql_ver}"
-                  $pxc_client = "percona-xtradb-cluster-client-${mysql::mysql_ver}"
-                }
-                $packs = [ $pxc_server, $pxc_client, "rsync" ]
+                $packs = [ "Percona-XtraDB-Cluster-server-${mysql::mysql_ver}", "Percona-XtraDB-Cluster-client-${mysql::mysql_ver}", "rsync" ]
                 $packs_galera = "Percona-XtraDB-Cluster-galera-${mysql::galera_version}.x"
 		$require = Package[$packs_galera]
 		$require_loc = Apt::Source['mysql-repo']
