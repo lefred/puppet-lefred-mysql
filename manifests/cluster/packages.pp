93186fb00a9807b502a412a011925f74d953ec90
Frédéric -lefred- Descamps <lefred@percona.com>
2014-07-15 13:16:41 +0200

    work on dep
    Signed-off-by: Frédéric -lefred- Descamps <lefred@percona.com>

--- manifests/cluster/packages.pp
+++ manifests/cluster/packages.pp
@@ -3,15 +3,31 @@ class mysql::cluster::packages {
   if  $mysql::mysql_distro == "mariadb" {
         $packs = [ "MariaDB-Galera-server", rsync ]
         $mysql_bin = "mysql"
-        $require = Yumrepo['mysql-repo'] 
+        case $::osfamily {
+    	  'RedHat': {
+               	$require = Yumrepo['mysql-repo'] 
+          }
+          'Debian': {
+		$require = Apt::Source['mysql-repo']
+          }
+        }
   } elsif  $mysql::mysql_distro == "percona" {
         $packs = [ "Percona-XtraDB-Cluster-server-${mysql::mysql_ver}", "Percona-XtraDB-Cluster-client-${mysql::mysql_ver}", "rsync", "qpress" ]
         $packs_galera = [ "Percona-XtraDB-Cluster-galera-${mysql::galera_version}", "Percona-Server-shared-compat" ]
         $mysql_bin = "mysql"
-        $require = [ Package[$packs_galera], Yumrepo['epel'] ]
+        case $::osfamily {
+    	  'RedHat': {
+                $require = [ Package[$packs_galera], Yumrepo['epel'] ]
+               	$require_loc = Yumrepo['mysql-repo'] 
+          }
+          'Debian': {
+		$require = Package[$packs_galera]
+		$require_loc = Apt::Source['mysql-repo']
+          }
+        }
         package {
             $packs_galera:
-                    require => Yumrepo['mysql-repo'],
+                    require => $require_loc,
                     ensure  => "installed";
         }
   }
